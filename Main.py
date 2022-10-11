7c420b4740395aaf19cd94ebe8d1580ed905fb74
URK21CS2002 <109648340+URK21CS2002@users.noreply.github.com>
2022-10-12 00:20:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,8 +17,8 @@ def merge_sort(data) -> None:
 
       while i < len(left) and j < len(right):
         if left[i] <= right[j]:
-            myList[k] = left[i]
-            i += 1
+               myList[k] = left[i]
+               i += 1
         else:
               myList[k] = right[j]
               j += 1
@@ -32,9 +32,9 @@ def merge_sort(data) -> None:
         k += 1
 
         while j < len(right):
-        myList[k]=right[j]
-        j += 1
-        k += 1
+          myList[k]=right[j]
+          j += 1
+          k += 1
 
 
 # Do not change the following code
